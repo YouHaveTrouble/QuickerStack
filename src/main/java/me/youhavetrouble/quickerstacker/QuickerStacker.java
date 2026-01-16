6665120e166a7871b1a844038a8bac139e26df4d
YouHaveTrouble <youhavetrouble@youhavetrouble.me>
2026-01-16 20:58:15 +0100

    actually register the interaction and test events

--- src/main/java/me/youhavetrouble/quickerstacker/QuickerStacker.java
+++ src/main/java/me/youhavetrouble/quickerstacker/QuickerStacker.java
@@ -1,19 +1,28 @@
 package me.youhavetrouble.quickerstacker;
 
 
+import com.hypixel.hytale.server.core.event.events.ecs.UseBlockEvent;
+import com.hypixel.hytale.server.core.event.events.player.PlayerInteractEvent;
+import com.hypixel.hytale.server.core.event.events.player.PlayerReadyEvent;
 import com.hypixel.hytale.server.core.modules.interaction.interaction.config.Interaction;
 import com.hypixel.hytale.server.core.plugin.JavaPlugin;
 import com.hypixel.hytale.server.core.plugin.JavaPluginInit;
 import me.youhavetrouble.quickerstacker.interaction.ChestInteraction;
+import me.youhavetrouble.quickerstacker.listener.ChestInteractListener;
 import org.checkerframework.checker.nullness.compatqual.NonNullDecl;
 
 public class QuickerStacker extends JavaPlugin {
 
     public QuickerStacker(@NonNullDecl JavaPluginInit init) {
         super(init);
+    }
+
+    @Override
+    public void setup() {
+        this.getEventRegistry().registerGlobal(UseBlockEvent.class, ChestInteractListener::onChestInteract);
+        this.getEventRegistry().registerGlobal(PlayerReadyEvent.class, ChestInteractListener::playerJoin);
 
         this.getCodecRegistry(Interaction.CODEC).register("YouHaveTrouble_QuickerStacker_QuickStackToChest", ChestInteraction.class, ChestInteraction.CODEC);
-
     }
 
 }
